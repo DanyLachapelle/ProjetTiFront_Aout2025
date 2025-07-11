a862e0fc7b4b6b57ca1c83f0c07443290e9226c2
DanyLachapelle <la228659@student.helha.be>
2025-07-12 00:15:48 +0200

    fix

---
+++ Groupe7TiFrontAout2025/src/app/features/manager/login-page/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private static URL: string = 'http://localhost:5201/api/users/';
+
+  private static LOGIN_URL: string = 'http://localhost:5201/api/users/login';
+
+  constructor(private _http:HttpClient) { }
+
+  login(loginData: { login: string; mot_de_passe: string }): Observable<any> {
+    return this._http.post<any>(UserService.LOGIN_URL, loginData);
+  }
+}
