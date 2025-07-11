c4a32de1abb83040aaf210f93343ce04d36c93d9
Guillaume Cardinal <la179896@student.helha.be>
2025-07-11 15:49:47 +0200

    liaison au back

---
+++ Groupe7TiFrontAout2025/src/app/services/mocktail.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+export interface Mocktail {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+  ingredients: Array<{
+    name: string;
+    quantity: number;
+    unit: string;
+  }>;
+}
+
+export interface Ingredient {
+  id: number;
+  name: string;
+  stock: number;
+  limit: number;
+  unit: string;
+}
+
+export interface CreateMocktailRequest {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  ingredients: Array<{
+    name: string;
+    quantity: number;
+    unit: string;
+  }>;
+}
+
+export interface UpdateMocktailRequest {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  ingredients: Array<{
+    name: string;
+    quantity: number;
+    unit: string;
+  }>;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MocktailService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Mocktail[]> {
+    return this.http.get<Mocktail[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Mocktail> {
+    return this.http.get<Mocktail>(`${this.apiUrl}/${id}`);
+  }
+
+  create(mocktail: CreateMocktailRequest): Observable<Mocktail> {
+    return this.http.post<Mocktail>(this.apiUrl, mocktail);
+  }
+
+  update(id: number, mocktail: UpdateMocktailRequest): Observable<Mocktail> {
+    return this.http.put<Mocktail>(`${this.apiUrl}/${id}`, mocktail);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  // Nouvelle méthode pour récupérer tous les ingrédients
+  getAllIngredients(): Observable<Ingredient[]> {
+    return this.http.get<Ingredient[]>(`${environment.apiUrl.replace('/mocktail', '')}/ingredient`);
+  }
+}
