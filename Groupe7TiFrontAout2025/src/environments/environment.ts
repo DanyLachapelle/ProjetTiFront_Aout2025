d0f7d152786327ccc2d68e7943a4672cdced5b17
DanyLachapelle <la228659@student.helha.be>
2025-08-14 17:09:57 +0200

    time out réglé normalement

--- Groupe7TiFrontAout2025/src/environments/environment.ts
+++ Groupe7TiFrontAout2025/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: "http://localhost:5201/api",
-  ingredientApiUrl: "http://localhost:5201/api/ingredients"
+  ingredientApiUrl: "http://localhost:5201/api/ingredients",
 }
